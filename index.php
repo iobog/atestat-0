f63d81e571886ffbf210104ff34a7cbdc53c9d57
ioan <ioan@ioan>
2023-04-19 21:21:10 +0300

    header

--- index.php
+++ index.php
@@ -1,5 +1,22 @@
 <html>
     <head>
-        
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="css/style.css">
     </head>
+    <body>
+        <header class="header">
+            <div class="logo">
+                un logo doar
+            </div>
+            <div class="navigation">
+                <ul>
+                    <span> <li><a href="index.html">Home</a></li>
+                    <icon-icon name=""></icon-icon>
+                    <li><a href="cos.php">Cos</a></li></span>
+                   
+                </ul>
+            </div>
+        </header>
+    </body>
 </html>
