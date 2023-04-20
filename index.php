19b993f99921ad487a6584fb6f0cdb4b50e843c1
ioan <ioan@ioan>
2023-04-20 21:49:50 +0300

    ?

--- index.php
+++ index.php
@@ -5,18 +5,6 @@
     <link rel="stylesheet" type="text/css" href="css/style.css">
     </head>
     <body>
-        <header class="header">
-            <div class="logo">
-                un logo doar
-            </div>
-            <div class="navigation">
-                <ul>
-                    <span> <li><a href="index.html">Home</a></li>
-                    <icon-icon name=""></icon-icon>
-                    <li><a href="cos.php">Cos</a></li></span>
-                   
-                </ul>
-            </div>
-        </header>
+        <?php include("headerIndex.php"); ?>
     </body>
 </html>
