5dd7aacd89b02956509235ac6fe85bdce15051ba
ioan <ioan@ioan>
2023-04-20 21:49:37 +0300

    some updates

---
+++ headerIndex.php
@@ -0,0 +1,23 @@
+<link rel="stylesheet" href="css/style.css" type="text/css">
+<header>
+    <div class="container-logo">
+        Website Logo
+    </div>
+    <div class="shopping-cart">
+        <a><img  src="shoppingcart.png"></a>
+
+    </div>
+</header>
+<nav class="navigation-bar">
+    <ul>
+        <li>
+            <a href="#">Parfumuri </a>
+            <ul>
+                <li><a href="#">Femei</a></li>
+                <li><a href="#">Barbati</a></li>
+            </ul>
+        </li>
+        <li><a href="#">About</a></li>
+        <li><a href="#">Contact</a></li>
+    </ul>
+</nav>
