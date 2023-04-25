7cc9088ded5e7368c7f08b62f4ca21bc60e0a511
ioan <ioan@ioan>
2023-04-25 23:42:56 +0300

    login_try

---
+++ login.php
@@ -0,0 +1,33 @@
+
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" type="text/css" href="css/loginpagestyle.css">
+
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link
+        href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap"
+        rel="stylesheet">
+
+      <script src="https://kit.fontawesome.com/401ef7ae9e.js" crossorigin="anonymous"></script>
+
+  </head>
+  <body>
+ 
+<!-- header and navigation -->
+    <div class="wrapper">
+      <h2 class="wrapper-title">Autentificare</h2>
+      <form  action="">
+          E-mail Address<br>
+        <input type="email" required name="adresa-email"><br>
+          Parola<br>
+        <input type="password" required name="password"><br>
+        <input type="submit" value="Autentificare" />
+      </form>
+    </div>
+
+
+  </body>
+</html>
